7b410235859556d633c4d4ac6228935cea1a2d1e
Tengxiao Yan <yantengxiao@gmail.com>
2018-11-13 15:11:40 -0600

    Fixed the bug of argument parse Update a new version of run.sh example

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python main.py --dataset ag_news --epochs 50 --batch_size 100 --initial_lr 0.0001 --depth vdcnn17 --optional_shortcut True --pooling maxpool
+python main.py --dataset yelp_review_full --lr_schedule --epochs 100 --batch_size 100 --initial_lr 0.0001 --depth vdcnn49 --pooling maxpool &
